5bc084129cbc3f3b49ac93a1363ffafbbe17ebfd
Yuriy <36164609+YurokBo@users.noreply.github.com>
2019-12-04 16:02:14 +0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+        return <header className='header'>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjtbT0GPAvCv_BaxTDO1AQkByLVbUYfJc_wv9X7sgfgxDUGK5P-Q&s" alt=""/>
+        </header>
+
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+        return <div className='content'>
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFocVdrh7XQ-XWjzCDgkUvEflBfBts5IxFoH2JhpjsAFj-O_PC&s" alt=""/>
+            </div>
+
+            <div>
+                ava + discrip
+            </div>
+
+            <div>
+                my posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+
+        </div>
+}
+
+export default Profile;
