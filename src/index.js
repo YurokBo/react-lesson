808f26842ff79f81d57fb25d255ab3848f9b1053
YurokBo <vertel3012@mail.ru>
2020-01-01 01:28:24 +0300

    delete render

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-/*import state from "./redux/State";
+import state, {updateNewPostText} from "./redux/State";
 import {addPost} from "./redux/State";
-import {BrowserRouter, Route} from "react-router-dom";*/
-import rerenderEntireTree from "./render";
-import state from "./redux/State";
+import {BrowserRouter, Route} from "react-router-dom";
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+        </BrowserRouter>,
+        document.getElementById('root'));
+};
+
+
 
 rerenderEntireTree(state);
 
